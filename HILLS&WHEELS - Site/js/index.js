f227ec94cfb251b6ee3f06ed66d4829a52692462
helloWorldRuan <ruan.rodrigues@sptech.school>
2022-11-05 13:14:31 -0300

    Criando carrosel e ajustes

---
+++ HILLS&WHEELS - Site/js/index.js
@@ -0,0 +1,17 @@
+window, addEventListener('scroll', function () {
+    var nav = document.querySelector('nav')
+    nav.classList.toggle('sticky', window.scrollY > 100)
+})
+
+function loader() {
+    $(window).on('load', () => {
+        setTimeout(() => {
+            $('.loader').fadeOut(1000);
+            $('main').fadeIn(1000)
+        }, 1000)
+    })
+}
+
+loader()
+
+
