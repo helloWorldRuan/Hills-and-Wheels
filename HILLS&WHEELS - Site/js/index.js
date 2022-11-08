5182fc5bb69b34e3ee17101408943a41dc47f957
helloWorldRuan <ruan.rodrigues@sptech.school>
2022-11-07 23:52:23 -0300

    Ajustes temporários nos vídeos

--- HILLS&WHEELS - Site/js/index.js
+++ HILLS&WHEELS - Site/js/index.js
@@ -1,8 +1,10 @@
+// Turn class header
 window.addEventListener('scroll', function () {
     var nav = document.querySelector('nav')
     nav.classList.toggle('sticky', window.scrollY > 100)
 })
 
+// Loader display on page initialization 
 function loader() {
     $(window).on('load', () => {
         setTimeout(() => {
@@ -11,7 +13,30 @@ function loader() {
         }, 1000)
     })
 }
-
 loader()
+
+function openVideo() {
+
+    var video = document.querySelector('.modal-video')
+
+    if (currentItem == 0){
+        video.style.opacity = 100
+        video.style.width = '100%'
+        fabio_video.style.width = '800px'
+        fabio_video.style.height = '450px'
+    }
+    
+
+    
+}
+
+function closeVideo() {
+    var video = document.querySelector('.modal-video')
+    video.style.opacity = 0
+    video.style.width = 0
+    fabio_video.style.width = 0
+    fabio_video.style.height = 0
+}
+
 
 
