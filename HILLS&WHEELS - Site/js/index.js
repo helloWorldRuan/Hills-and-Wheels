3826e30f69ca09f229a8bcec7fa8c4906aecb662
helloWorldRuan <ruan.rodrigues@sptech.school>
2022-11-11 13:24:24 -0300

    Implementação final do carrossel

--- HILLS&WHEELS - Site/js/index.js
+++ HILLS&WHEELS - Site/js/index.js
@@ -15,28 +15,7 @@ function loader() {
 }
 loader()
 
-function openVideo() {
 
-    var video = document.querySelector('.modal-video')
-
-    if (currentItem == 0){
-        video.style.opacity = 100
-        video.style.width = '100%'
-        fabio_video.style.width = '800px'
-        fabio_video.style.height = '450px'
-    }
-    
-
-    
-}
-
-function closeVideo() {
-    var video = document.querySelector('.modal-video')
-    video.style.opacity = 0
-    video.style.width = 0
-    fabio_video.style.width = 0
-    fabio_video.style.height = 0
-}
 
 
 
