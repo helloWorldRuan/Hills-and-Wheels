d885b47144b0c9de96cdfec4c2ed726e84648ea0
helloWorldRuan <ruan.rodrigues@sptech.school>
2022-11-25 01:24:05 -0300

    Atualizando consultas

--- BD/HillsWheels.sql
+++ BD/HillsWheels.sql
@@ -39,7 +39,6 @@ fkConquista INT,
 FOREIGN KEY (fkConquista) REFERENCES Conquista (idConquista)
 );
 
-
 SELECT * FROM Conquista;
 SELECT * FROM Biker;
 SELECT * FROM Conquista;
@@ -50,8 +49,37 @@ JOIN Modalidade m ON b.fkModalidade = m.idModalidade
 JOIN Conquista c ON b.fkConquista = c.idConquista;
 
 -- Quantidade de usuários (Bikers) | Kilometros totais percorridos | Quantidade de países distintos
-SELECT COUNT(idBiker) Bikers, COUNT(pais) Paises, SUM(distancia) KM FROM Biker
-JOIN Conquista ON fkConquista = idCOnquista;
+SELECT  COUNT(idBiker) Bikers, COUNT(DISTINCT pais) Paises, SUM(distancia) KM FROM Biker
+JOIN Conquista ON fkConquista = idConquista;
+
+-- Quantidade de público por gênero
+SELECT COUNT(genero) gender  FROM Biker
+GROUP BY genero;
+
+-- Quantidade de público por modalidade
+SELECT m.nome as modal, COUNT(fkModalidade) bikers FROM Biker
+JOIN Modalidade as m ON fkModalidade = idModalidade
+GROUP BY m.nome;
+
+-- Quantidade de público por faixa etária
+SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) AS '10-20 ANOS' FROM Biker
+WHERE YEAR(dtNasc) >= 2000 AND YEAR(dtNasc) <= 2010
+UNION
+SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) AS '20-30 ANOS' FROM Biker
+WHERE YEAR(dtNasc) >= 1990 AND YEAR(dtNasc) <= 2000
+UNION
+SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) AS '30-40 ANOS' FROM Biker
+WHERE YEAR(dtNasc) >= 1980 AND YEAR(dtNasc) <= 1990
+UNION
+SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) AS '40-50 ANOS' FROM Biker
+WHERE YEAR(dtNasc) >= 1970 AND YEAR(dtNasc) <= 1980
+UNION
+SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) AS '50-60 ANOS' FROM Biker
+WHERE YEAR(dtNasc) >= 1960 AND YEAR(dtNasc) <= 1970
+UNION
+SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) AS '60-70 ANOS' FROM Biker
+WHERE YEAR(dtNasc) >= 1950 AND YEAR(dtNasc) <= 1960;
+
 
 
 
