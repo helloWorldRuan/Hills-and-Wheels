04fc79b789df58b20e731491659fb9cdf307beb3
helloWorldRuan <ruan.rodrigues@sptech.school>
2022-11-26 16:37:14 -0300

    Ajustando select do gráfico de faixa etária

--- SITE/src/models/usuarioModel.js
+++ SITE/src/models/usuarioModel.js
@@ -27,7 +27,7 @@ function selectData(filtro, order) {
     console.log(filtro, order);
 
     var instrucao =
-    `
+        `
     SELECT  b.idBiker, b.nome, m.nome modalidade, c.* FROM Biker b 
     JOIN Modalidade m ON b.fkModalidade = m.idModalidade 
     JOIN Conquista c ON b.fkConquista = c.idConquista
@@ -38,18 +38,18 @@ function selectData(filtro, order) {
 }
 
 // Select dados dos insights
-function selectInsights(){
+function selectInsights() {
     var instrucao = `
         SELECT COUNT(idBiker) Bikers, COUNT(DISTINCT pais) Paises, SUM(distancia) KM 
         FROM Biker JOIN Conquista ON fkConquista = idConquista;`
 
     console.log(instrucao)
-    
+
     return database.executar(instrucao)
 }
 
 // Select dados do gráfico de público por gênero
-function selectPublicGender(){
+function selectPublicGender() {
     var instrucao = "SELECT COUNT(genero) gender FROM Biker GROUP BY genero;"
     console.log(instrucao)
 
@@ -57,7 +57,7 @@ function selectPublicGender(){
 }
 
 // Select dados do gráfico de público por modalidade
-function selectPublicModal(){
+function selectPublicModal() {
     var instrucao = `
         SELECT m.nome as modal, COUNT(fkModalidade) bikers FROM Biker
         JOIN Modalidade as m ON fkModalidade = idModalidade
@@ -69,25 +69,16 @@ function selectPublicModal(){
 }
 
 // Select dados do gráfico de público por idade
-function selectPublicAge(){
+function selectPublicAge() {
     var instrucao = `
-        SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) AS 'idade' FROM Biker
-        WHERE YEAR(dtNasc) >= 2000 AND YEAR(dtNasc) <= 2010
-        UNION
-        SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) FROM Biker
-        WHERE YEAR(dtNasc) >= 1990 AND YEAR(dtNasc) <= 2000
-        UNION
-        SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) FROM Biker
-        WHERE YEAR(dtNasc) >= 1980 AND YEAR(dtNasc) <= 1990
-        UNION
-        SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) FROM Biker
-        WHERE YEAR(dtNasc) >= 1970 AND YEAR(dtNasc) <= 1980
-        UNION
-        SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) FROM Biker
-        WHERE YEAR(dtNasc) >= 1960 AND YEAR(dtNasc) <= 1970
-        UNION
-        SELECT COUNT(TIMESTAMPDIFF(YEAR, dtNasc, CURDATE())) FROM Biker
-        WHERE YEAR(dtNasc) >= 1950 AND YEAR(dtNasc) <= 1960;
+        SELECT 
+        IF(YEAR(dtNasc) >= 2000, '10-20',  IF(YEAR(dtNasc) >= 1990, '20-30', 
+        IF(YEAR(dtNasc) >= 1980, '30-40', 
+        IF(YEAR(dtNasc) >= 1970, '40-50', 
+        IF(YEAR(dtNasc) >= 1960, '50-60', 
+        IF(YEAR(dtNasc) >= 1950, '60-70', 0)))))) "age", nome 
+        FROM Biker
+        ORDER BY dtNasc;
     `
 
     return database.executar(instrucao)
